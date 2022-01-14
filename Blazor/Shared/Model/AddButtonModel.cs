6244b168eb39f09ad191cb2b719bbc5a90ea7eae
Григорий Чайко <g.chaiko@cbt.by>
2022-01-14 15:59:43 +0300

    #Docker start

---
+++ Blazor/Shared/Model/AddButtonModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor.Shared.Model
+{
+    public class AddButtonModel
+    {
+        public int PostId { get; set; }
+        public string OperationType { get; set; } = string.Empty;
+    }
+}
