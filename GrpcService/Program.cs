742ef76d1655d48a6a2a902a2166710b460ba95d
RyhoR <7755804@gmail.com>
2022-02-01 00:17:50 +0300

    #grpc docker

--- GrpcService/Program.cs
+++ GrpcService/Program.cs
@@ -2,7 +2,9 @@ using GrpcService.Data;
 using GrpcService.Interface;
 using GrpcService.Services;
 using GrpcService.ServicesDB;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.EntityFrameworkCore;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,11 +14,24 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddGrpc();
 
+builder.WebHost.UseKestrel(options =>
+{
+    // configure Kestrel with our HTTPS certificate
+    //options.ConfigureHttpsDefaults(ConfigureHttps);
+    //options.Listen(IPAddress.Loopback, 7298);
+    //// gRPC
+    //options.ListenAnyIP(443, o => o.Protocols = HttpProtocols.Http2);
+    //// HTTP
+    //options.ListenAnyIP(5298, o => o.Protocols = HttpProtocols.Http1);
+});
+
 builder.Services.AddDbContext<MessagesDBConext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddTransient<IMessagesServices, MessagesServices>();
 
 var app = builder.Build();
+
+app.UseHttpsRedirection();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<MessagesService>();
@@ -24,5 +39,5 @@ app.MapGrpcService<MessagesService>();
 
 
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-app.MigrateDatabase();
+//app.MigrateDatabase();
 app.Run();
